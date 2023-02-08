0febd71e8d757dc78ce12f4395c01ecbea20d920
Farid Mammadbayov <farid.mammadbayov@yrbrands.com>
2023-02-08 15:06:31 +0100

    lesson 19 pre node

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,30 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
-}
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Web Warrior",
+    description: "Web dev portfolio",
+    copyright: "This website is copyright 2021 Web Warrior",
+    contact: "me@thewebwarrioruk.com",
+  },
+};
---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam
+        </p>
+      </div>
+    </Layout>
+  );
+}
